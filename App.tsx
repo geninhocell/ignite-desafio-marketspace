5184385efe1fbfb589cda95a64ef211e91f10d80
geninhocell <geninhocell@gmail.com>
2022-12-03 14:47:50 -0300

    chore: add font

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
+import {
+  Karla_400Regular,
+  Karla_700Bold,
+  useFonts,
+} from '@expo-google-fonts/karla';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Karla_400Regular,
+    Karla_700Bold,
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
